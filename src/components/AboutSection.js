be885fe76135b49affdf83f7fa3581381ec67b50
Joaquim <jogi@itu.dk>
2020-11-23 17:48:28 +0100

    Add wave and About Us animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,52 +1,38 @@
+import React from "react";
 import home1 from "../img/home1.png";
-
-import { About, Description, Hide, Image } from "../styles";
-
+import { About, Description, Image, Hide } from "../styles";
+//Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../Animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 2 } },
-  };
-
-  const container = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: { duration: 0.75, ease: "easeOut", staggerChildren: 1 },
-    },
-  };
   return (
     <About>
       <Description>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="title"
-        >
+        <motion.div>
           <Hide>
             <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
             <motion.h2 variants={titleAnim}>
-              your <span>dreams</span>
+              your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>come true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="Photographer" />
+        <motion.img variants={photoAnim} src={home1} alt="Photographer" />
       </Image>
+      <Wave />
     </About>
   );
 };
